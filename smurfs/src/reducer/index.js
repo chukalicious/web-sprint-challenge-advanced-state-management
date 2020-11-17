c56e259ed461469f7963a4ec5667a58095c120c7
Katiuska <katiuskaalicea@gmail.com>
2020-11-16 22:07:48 -0800

    First commit

---
+++ smurfs/src/reducer/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  isLoading: false,
+};
+
+export const villageReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
